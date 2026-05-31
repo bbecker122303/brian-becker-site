e2a00df9d97d4cbc662fcbd308807f4fee06ef64
bbecker122303 <163230125+bbecker122303@users.noreply.github.com>
2026-05-30 20:34:33 -0400

    Update admin password via admin portal

--- admin/config.js
+++ admin/config.js
@@ -2,5 +2,6 @@ window.ADMIN_CONFIG = {
     passwordHash: 'bde14335299b7c116e55eed5b54e2d90665b35672b47996a4151efb315b1c1fb',
     repo: 'bbecker122303/brian-becker-site',
     branch: 'main',
-    contentPath: 'content/site.json'
+    contentPath: 'content/site.json',
+    encryptedToken: {"salt":"FrYOLA9+NlxU5PqTrCEfAA==","iv":"tAis9Exle0a1jwuV","data":"MlItv9k35rIZa1RPSwxBnpAEiIuqS2G98+27iCgdJlLv4BO5R4zXalNvtQJAtfTirTKd5fZepHDPh+EpD0v8s8WAq6qS0OH8OdO3mn0vQ7zjCaqUgSYVzPS5BtgjAp4Zed59HZxiromeSejBQQ=="}
 };
